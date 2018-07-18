7eaa0637ae7bee5677e4b4006c9d77e4838ec5f6
neilmarshall <neil.marshall@dunelm.org.uk>
2018-07-19 00:09:06 +0100

    Update for foul balls - first working commit

--- frame_generator.py
+++ frame_generator.py
@@ -20,6 +20,7 @@ def generate_pair_of_pins(last_pair=False):
         selected from interval [0, ..., 10]
     """
     possible_rolls = list(range(11)) + ['F']
+
     first_ball = random.choices(population=possible_rolls,
                                 weights=[1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 5, 2],
                                 k=1).pop()
@@ -30,16 +31,16 @@ def generate_pair_of_pins(last_pair=False):
         second_ball = random.choice(possible_rolls)
     else:
         second_ball = random.choice([10 - first_ball,
-                                     random.randrange(0, 10 - first_ball,
-                                     'F')])
+                                     random.randrange(0, 10 - first_ball),
+                                     'F'])
 
-    if last_pair and (first_ball == 10 or first_ball + second_ball == 10):
-        if first_ball == 10: 
-            fill_ball = random.choice(possible_rolls)
-        elif first_ball != 'F' and second_ball != 'F' and first_ball + second_ball == 10:
-            fill_ball = random.choice(possible_rolls)
-            return (first_ball, second_ball, fill_ball)
-    return (first_ball, second_ball)
+    total = sum(x for x in (first_ball, second_ball) if x is not None and x != 'F')
+
+    if last_pair and total >= 10:
+        fill_ball = random.choice(possible_rolls)
+        return (first_ball, second_ball, fill_ball)
+    else:
+        return (first_ball, second_ball)
 
 
 def generate_frame():
@@ -49,40 +50,79 @@ def generate_frame():
     return [generate_pair_of_pins() for _ in range(9)] + [generate_pair_of_pins(True)]
     
 
-@mock.patch('random.randint')
 @mock.patch('random.choice')
 @mock.patch('random.choices')
 class TestGeneratePairs(unittest.TestCase):
 
-    def test_generate_basic(self, mock_choices, mock_choice, mock_randint):
+    def test_generate_basic(self, mock_choices, mock_choice):
         mock_choices.return_value = [5]
         mock_choice.return_value = 4
         self.assertTupleEqual(generate_pair_of_pins(), (5, 4))
 
-    def test_generate_strike(self, mock_choices, mock_choice, mock_randint):
+    def test_generate_strike(self, mock_choices, mock_choice):
         mock_choices.return_value = [10]
         self.assertTupleEqual(generate_pair_of_pins(), (10, None))
 
-    def test_generate_spare(self, mock_choices, mock_choice, mock_randint):
+    def test_generate_spare(self, mock_choices, mock_choice):
         mock_choices.return_value = [8]
         mock_choice.return_value = 2
         self.assertTupleEqual(generate_pair_of_pins(), (8, 2))
 
-    def test_generate_basic_as_last_pair(self, mock_choices, mock_choice, mock_randint):
+    def test_generate_basic_as_last_pair(self, mock_choices, mock_choice):
         mock_choices.return_value = [5]
         mock_choice.return_value = 4
         self.assertTupleEqual(generate_pair_of_pins(True), (5, 4))
 
-    def test_generate_strike_as_last_pair(self, mock_choices, mock_choice, mock_randint):
+    def test_generate_strike_as_last_pair(self, mock_choices, mock_choice):
         mock_choices.return_value = [10]
-        mock_randint.side_effect = [4, 6]
+        mock_choice.side_effect = [4, 6]
         self.assertTupleEqual(generate_pair_of_pins(True), (10, 4, 6))
 
-    def test_generate_spare_as_last_pair(self, mock_choices, mock_choice, mock_randint):
+    def test_generate_spare_as_last_pair(self, mock_choices, mock_choice):
         mock_choices.return_value = [8]
-        mock_choice.return_value = 2
-        mock_randint.return_value = 3
+        mock_choice.side_effect = [2, 3]
         self.assertTupleEqual(generate_pair_of_pins(True), (8, 2, 3))
+
+    def test_generate_basic_with_two_fouls(self, mock_choices, mock_choice):
+        mock_choices.return_value = ['F']
+        mock_choice.return_value = 'F'
+        self.assertTupleEqual(generate_pair_of_pins(), ('F', 'F'))
+
+    def test_generate_basic_with_foul_first_ball(self, mock_choices, mock_choice):
+        mock_choices.return_value = ['F']
+        mock_choice.return_value = 4
+        self.assertTupleEqual(generate_pair_of_pins(), ('F', 4))
+
+    def test_generate_basic_with_foul_second_ball(self, mock_choices, mock_choice):
+        mock_choices.return_value = [7]
+        mock_choice.return_value = 'F'
+        self.assertTupleEqual(generate_pair_of_pins(), (7, 'F'))
+
+    def test_generate_spare_with_foul_first_ball(self, mock_choices, mock_choice):
+        mock_choices.return_value = ['F']
+        mock_choice.return_value = 10
+        self.assertTupleEqual(generate_pair_of_pins(), ('F', 10))
+
+    def test_generate_basic_with_foul_first_ball_as_last_pair(self, mock_choices, mock_choice):
+        mock_choices.return_value = ['F']
+        mock_choice.return_value = 4
+        self.assertTupleEqual(generate_pair_of_pins(True), ('F', 4))
+
+    def test_generate_basic_with_foul_second_ball_as_last_pair(self, mock_choices, mock_choice):
+        mock_choices.return_value = [7]
+        mock_choice.return_value = 'F'
+        self.assertTupleEqual(generate_pair_of_pins(True), (7, 'F'))
+
+    def test_generate_spare_with_foul_first_ball_as_last_pair(self, mock_choices, mock_choice):
+        mock_choices.return_value = ['F']
+        mock_choice.side_effect = [10, 5]
+        self.assertTupleEqual(generate_pair_of_pins(True), ('F', 10, 5))
+
+
+    def test_generate_strike_with_subsequent_fouls(self, mock_choices, mock_choice):
+        mock_choices.return_value = [10]
+        mock_choice.side_effect = ['F', 'F']
+        self.assertTupleEqual(generate_pair_of_pins(True), (10, 'F', 'F'))
 
 
 class TestGenerateFrame(unittest.TestCase):
@@ -101,15 +141,15 @@ class TestGenerateFullTests(unittest.TestCase):
     
     def test_generate_pair_of_pins_complete_no_last_pair(self):
         pairs = [generate_pair_of_pins() for _ in range(6)]
-        self.assertListEqual(pairs, [(10, None), (10, None), (7, 1), (7, 1), (6, 4), (5, 0)])
+        self.assertListEqual(pairs, [(10, None), (10, None), (7, 'F'), (8, 1), (6, 'F'), (3, 7)])
 
     def test_generate_pair_of_pins_complete_with_last_pair(self):
         pairs = [generate_pair_of_pins(True) for _ in range(6)]
-        self.assertListEqual(pairs, [(10, 6, 0), (5, 4), (7, 1), (6, 4, 4), (2, 1), (10, 8, 9)])
+        self.assertListEqual(pairs, [(10, 6, 0), (5, 4), (7, 1), (6, 'F'), (3, 7, 9), (10, 8, 'F')])
 
     def test_generate_frame_complete(self):
         frame = generate_frame()
-        self.assertListEqual(frame, [(10, None), (10, None), (7, 1), (7, 1), (6, 4), (5, 0), (10, None), (8, 0), (1, 1), (7, 0)])
+        self.assertListEqual(frame, [(10, None), (10, None), (7, 'F'), (8, 1), (6, 'F'), (3, 7), (9, 'F'), ('F', 9), (10, None), (6, 'F')])
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -7,7 +7,8 @@ def score_game(frame):
     Args : frame (list)
         List of pins as tuples indicating score on first and second
         balls. If a strike is achieved (i.e. score on first pin is
-        10) then second ball will be 'None'.
+        10) then second ball will be 'None'. Foul balls will be
+        denoted as 'F'.
 
     Basic test with no strikes / spares:
         Note: Frame scores = [9, 8, 1, 7, 9, 0, 6, 5, 5, 7]
@@ -45,22 +46,31 @@ def score_game(frame):
                     (10, None), (10, None), (10, None), (10, None), (10, 10, 10)])
     300
 
-    Test first frame produced by frame generator:
+    Test frame produced by frame generator:
         Note: Frame scores = [27, 18, 8, 8, 15, 5, 18, 8, 2, 7]
     >>> score_game([(10, None), (10, None), (7, 1), (7, 1), (6, 4), (5, 0), \
                     (10, None), (8, 0), (1, 1), (7, 0)])
     116
+
+    Test frame with foul balls:
+        Note: Frame scores = [27, 17, 7, 9, 6, 19, 9, 9, 16, 6]
+    >>> score_game([(10, None), (10, None), (7, 'F'), (8, 1), (6, 'F'), \
+                    (3, 7), (9, 'F'), ('F', 9), (10, None), (6, 'F')])
+    125
     """
+    def replace_fouls(pair):
+        return tuple(x if x != 'F' else 0 for x in pair)
     def score_pair(pair1, pair2, pair3):
+        pair1, pair2, pair3 = map(replace_fouls, (pair1, pair2, pair3))
         if pair1 == (10, None):
             return 10 + pair2[0] + (pair2[1] if pair2[1] is not None else pair3[0])  # score strikes
         elif len(pair1) == 2 and sum(pair1) == 10:
             return 10 + pair2[0]  # score spares
         else:
             return sum(pair1)  # scores non-strikes/spares
-    return sum([score_pair(p1, p2, p3) for p1, p2, p3 in zip_longest(frame, frame[1:], frame[2:])])
+    return sum([score_pair(p1, p2, p3) for p1, p2, p3 in zip_longest(frame, frame[1:], frame[2:], fillvalue=(None, None))])
 
 
 if __name__ == '__main__':
-    import doctest; doctest.testmod()
+    import doctest; doctest.testmod(verbose=True)
 
