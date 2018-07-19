c35ac361daff7973e26b3243e8a1fe6d82a5ed57
Neil <neil.marshall@dunelm.org.uk>
2018-07-19 12:54:48 +0100

    Add 'score_match' method
    Add method 'score_match' and doctests.
    
    Update name of 'score_game' method to 'score_frame'.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3.6
+score_frame#!/usr/bin/env python3.6
 
 from itertools import zip_longest
 
-def score_game(frame):
+def score_frame(frame):
     """
     Args : frame (list)
         List of pins as tuples indicating score on first and second
@@ -12,49 +12,49 @@ def score_game(frame):
 
     Basic test with no strikes / spares:
         Note: Frame scores = [9, 8, 1, 7, 9, 0, 6, 5, 5, 7]
-    >>> score_game([(8, 1), (8, 0), (1, 0), (6, 1), (6, 3), \
+    >>> score_frame([(8, 1), (8, 0), (1, 0), (6, 1), (6, 3), \
                     (0, 0), (0, 6), (4, 1), (4, 1), (5, 2)])
     57
 
     Test including strikes / spares but not ending in a strike / spare:
         Note: Frame scores = [18, 8, 17, 7, 10, 10, 6, 20, 17, 7]
-    >>> score_game([(8, 2), (8, 0), (10, None), (6, 1), (7, 3), \
+    >>> score_frame([(8, 2), (8, 0), (10, None), (6, 1), (7, 3), \
                     (0, 10), (0, 6), (9, 1), (10, None), (5, 2)])
     120
 
     Test with frame ending in a strike, resulting in 2 fill balls
         Note: Frame scores = [20, 19, 9, 18, 8, 10, 6, 30, 28, 19]
-    >>> score_game([(10, None), (7, 3), (9, 0), (10, None), (0, 8), \
+    >>> score_frame([(10, None), (7, 3), (9, 0), (10, None), (0, 8), \
                     (8, 2), (0, 6), (10, None), (10, None), (10, 8, 1)])
     167
 
     Test with frame ending in a spare, resulting in a fill ball
         Note: Frame scores = [20, 19, 9, 18, 8, 10, 6, 29, 20, 11]
-    >>> score_game([(10, None), (7, 3), (9, 0), (10, None), (0, 8), \
+    >>> score_frame([(10, None), (7, 3), (9, 0), (10, None), (0, 8), \
                     (8, 2), (0, 6), (10, None), (10, None), (9, 1, 1)])
     150
 
     Test with frame ending in a spare, resulting in a fill ball, which is missed
         Note: Frame scores = [20, 19, 9, 18, 8, 10, 6, 29, 20, 10]
-    >>> score_game([(10, None), (7, 3), (9, 0), (10, None), (0, 8), \
+    >>> score_frame([(10, None), (7, 3), (9, 0), (10, None), (0, 8), \
                     (8, 2), (0, 6), (10, None), (10, None), (9, 1, 0)])
     149
 
     Test perfect game:
         Note: Frame scores = [30, 30, 30, 30, 30, 30, 30, 30, 30, 30]
-    >>> score_game([(10, None), (10, None), (10, None), (10, None), (10, None), \
+    >>> score_frame([(10, None), (10, None), (10, None), (10, None), (10, None), \
                     (10, None), (10, None), (10, None), (10, None), (10, 10, 10)])
     300
 
     Test frame produced by frame generator:
         Note: Frame scores = [27, 18, 8, 8, 15, 5, 18, 8, 2, 7]
-    >>> score_game([(10, None), (10, None), (7, 1), (7, 1), (6, 4), (5, 0), \
+    >>> score_frame([(10, None), (10, None), (7, 1), (7, 1), (6, 4), (5, 0), \
                     (10, None), (8, 0), (1, 1), (7, 0)])
     116
 
     Test frame with foul balls:
         Note: Frame scores = [27, 17, 7, 9, 6, 19, 9, 9, 16, 6]
-    >>> score_game([(10, None), (10, None), (7, 'F'), (8, 1), (6, 'F'), \
+    >>> score_frame([(10, None), (10, None), (7, 'F'), (8, 1), (6, 'F'), \
                     (3, 7), (9, 'F'), ('F', 9), (10, None), (6, 'F')])
     125
     """
@@ -83,6 +83,72 @@ def calculate_total_pins(series):
     300
     """
     return sum(sum(roll for turn in frame for roll in turn if roll is not None and roll != 'F') for frame in series)
+
+
+def score_match(series):
+    """
+    A match between two players consists of three frames, collectively called a series.
+
+    For each frame, the player with the higher score receives two points (or both players
+    receive a point eac if their scores are tied).
+
+    In addition, the player with the most pins knocked down in total across all three
+    frames (bonus points for strikes and spares do NOT count, but fill balls do) receives
+    an additional point. No points are awarded here in the event of a tie.
+
+    Function returns who won the match.
+
+    >>> frame1 = ([(10, None), (7, 3), (9, 0), (10, None), (0, 8), \
+                   (8, 2), (0, 6), (10, None), (10, None), (9, 1, 1)])
+    >>> frame2 = ([(8, 2), (8, 0), (10, None), (6, 1), (7, 3), \
+                   (0, 10), (0, 6), (9, 1), (10, None), (5, 2)])
+    >>> frame3 = ([(10, None), (7, 3), (9, 0), (10, None), (0, 8), \
+                   (8, 2), (0, 6), (10, None), (10, None), (10, 8, 1)])
+
+    >>> frameA = ([(8, 1), (8, 0), (1, 0), (6, 1), (6, 3), \
+                   (0, 0), (0, 6), (4, 1), (4, 1), (5, 2)])
+    >>> frameB = ([(10, None), (10, None), (7, 'F'), (8, 1), (6, 'F'), \
+                   (3, 7), (9, 'F'), ('F', 9), (10, None), (6, 'F')])
+    >>> frameC = ([(10, None), (7, 3), (9, 0), (10, None), (0, 8), \
+                   (8, 2), (0, 6), (10, None), (10, None), (10, 8, 1)])
+
+    Scores for the frames are ([150, 120, 167], [57, 125, 167])
+    Total pins knocked down are (284, 245)
+    Points are therefore {Player 1: [2, 0, 1, 1], Player 2: [0, 2, 1, 0]}
+
+    >>> score_match(((frame1, frame2, frame3), (frameA, frameB, frameC)))
+    'Player 1 won!'
+    """
+    scores = {i: tuple(score_frame(frame) for frame in series[i]) for i in range(2)}
+
+    points = {i: [] for i in range(2)}
+
+    for frame in range(3):
+        if scores[0][frame] > scores[1][frame]:
+            points[0].append(2)
+            points[1].append(0)
+        elif scores[0][frame] < scores[1][frame]:
+            points[0].append(0)
+            points[1].append(2)
+        else:
+            points[0].append(1)
+            points[1].append(1)
+
+    total_pins = {i: calculate_total_pins(series[i]) for i in range(2)}
+
+    if total_pins[0] > total_pins[1]:
+        points[0].append(1)
+        points[1].append(0)
+    elif total_pins[0] < total_pins[1]:
+        points[0].append(0)
+        points[1].append(1)
+
+    if sum(points[0]) > sum(points[1]):
+        return "Player 1 won!"
+    elif sum(points[0]) < sum(points[1]):
+        return "Player 2 won!"
+    else:
+        return "The match is a draw!"
 
 
 if __name__ == '__main__':
